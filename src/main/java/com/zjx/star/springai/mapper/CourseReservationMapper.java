f3c44f1523434b8697573c5fd47a74a2c04cfb1e
xywyhmily <15282167445@163.com>
2025-04-30 20:37:25 +0800

    智能客服

---
+++ src/main/java/com/zjx/star/springai/mapper/CourseReservationMapper.java
@@ -0,0 +1,16 @@
+package com.zjx.star.springai.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.zjx.star.springai.entity.po.CourseReservation;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author 
+ * @since 2025-04-30
+ */
+public interface CourseReservationMapper extends BaseMapper<CourseReservation> {
+
+}
---
+++ src/main/java/com/zjx/star/springai/mapper/SchoolMapper.java
@@ -0,0 +1,16 @@
+package com.zjx.star.springai.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.zjx.star.springai.entity.po.School;
+
+/**
+ * <p>
+ * 校区表 Mapper 接口
+ * </p>
+ *
+ * @author 
+ * @since 2025-04-30
+ */
+public interface SchoolMapper extends BaseMapper<School> {
+
+}
